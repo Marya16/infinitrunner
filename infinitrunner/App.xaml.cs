7bf6dc28dcaf6a3e8406dec49de7c38f36d29411
Marya <maryaeduarda15059927@gmail.com>
2024-11-01 10:37:06 -0300

    uyyfg

---
+++ infinitrunner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace infinitrunner;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ infinitrunner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace infinitrunner;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
